7f149eba1d1decadc79b9a553f6421f18d7de712
Victor Bianchi <vbianchi90@gmail.com>
2020-01-25 16:57:25 -0500

    create Task model, controller, routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "tasks", to: "tasks#index"
+  get "task/new", to: "tasks#new"
+  get "task/:id", to: "tasks#show"
+  post "tasks", to: "tasks#create"
+  get "task/:id/edit", to: "tasks#edit"
+  patch "task/:id", to: "tasks#update"
+  delete "task/:id", to: "tasks#destroy"
 end

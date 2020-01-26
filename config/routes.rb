a3e8bfe4cb52b52b7487559f93efcec2cbaae645
Victor Bianchi <vbianchi90@gmail.com>
2020-01-26 11:27:42 -0500

    implement CRUD actions for Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
 
   get "tasks", to: "tasks#index"
   get "task/new", to: "tasks#new"
-  get "task/:id", to: "tasks#show"
+  get "task/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
-  get "task/:id/edit", to: "tasks#edit"
+  get "task/:id/edit", to: "tasks#edit", as: :edit
   patch "task/:id", to: "tasks#update"
   delete "task/:id", to: "tasks#destroy"
 end
